aca77882544afc4c5dbf05166f0f3073e77d7d8b
wailingwhale111 <115021551+wailingwhale111@users.noreply.github.com>
2022-12-16 13:48:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -71,61 +71,6 @@ function App() {
               totalNum={totalNum}
               setTotalNum={setTotalNum}
               biasList={biasList}/>
-            {/* <Card>
-              <Card.Body>
-                <DropdownButton
-                  id="dropdown-button-dark-example1"
-                  variant="secondary"
-                  menuVariant="dark"
-                  title="Sort by:"
-                  className="mt-2"
-                  onSelect={selectSorting}>
-                  <Dropdown.Item eventKey="Age"> Age </Dropdown.Item>
-                  <Dropdown.Item eventKey="Alphabetical"> Alphabetical </Dropdown.Item>
-                </DropdownButton>
-                <DropdownButton 
-                  id="dropdown-button-dark-example2"
-                  variant="secondary"
-                  menuVariant="dark"
-                  title="Filter by Position:"
-                  className="mt-2"
-                  onSelect={selectFilterPosition}>
-                  <Dropdown.Item eventKey="All"> All </Dropdown.Item>
-                  <Dropdown.Item eventKey="Vocalist">Vocalist</Dropdown.Item>
-                  <Dropdown.Item eventKey="Rapper">Rapper</Dropdown.Item>
-                  <Dropdown.Item eventKey="Dancer">Dancer</Dropdown.Item>
-                  <Dropdown.Item eventKey="Leader">Leader</Dropdown.Item>
-                </DropdownButton>
-                <DropdownButton
-                  id="dropdown-button-dark-example3"
-                  variant="secondary"
-                  menuVariant="dark"
-                  title="Filter by SubGroups:"
-                  className="mt-2"
-                  onSelect={selectFilterGroup}>
-                  <Dropdown.Item eventKey="All"> All </Dropdown.Item>
-                  <Dropdown.Item eventKey="NCT DREAM">NCT DREAM</Dropdown.Item>
-                  <Dropdown.Item eventKey="NCT 127">NCT 127</Dropdown.Item>
-                  <Dropdown.Item eventKey="NCT U">NCT U</Dropdown.Item>
-                  <Dropdown.Item eventKey="WayV">WayV</Dropdown.Item>
-                  <Dropdown.Item eventKey="NCT 2018">NCT 2018</Dropdown.Item>
-                  <Dropdown.Item eventKey="NCT 2020">NCT 2020</Dropdown.Item>
-                  <Dropdown.Item eventKey="NCT 2021">NCT 2021</Dropdown.Item>
-                </DropdownButton>
-                <br/>
-                <strong>My Bias List:</strong> 
-                {biasList.map(({name, age}) => {
-                  total += age
-                    return (
-                    <ListGroup.Item>
-                      {name}: {age}
-                    </ListGroup.Item>
-                  )})}
-                  <br/>
-                <Button className="btn btn-danger" type="button" onClick={handleClearFilter}>Clear Filter</Button>
-                </Card.Body>
-                <Card.Footer>Average Age of My Bias: {totalNum === 0 ? "" : total / totalNum}</Card.Footer>
-              </Card> */}
           </Col>
           <Col>
             <h1><strong>My NCT Universe</strong></h1> 
